98f2012df6a39f4499a17332e5fef97911c357a0
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-20 16:53:10 -0400

    Added the GitHub urls

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/WickedZ2309" target="_blank">
+          Andrea G.
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/WickedZ2309/shecodes-react-week5-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
