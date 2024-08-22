29faece95931a2010a6054d0510111414b1c4cf3
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-21 23:51:42 -0400

    Correction of  some code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Weather from "./Weather";
-
 import "./App.css";
 
 export default function App() {
@@ -8,12 +7,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Montreal" />
+
         <footer>
           This project was coded by{" "}
           <a
             href="https://github.com/WickedZ2309"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             Andrea G.
           </a>{" "}
@@ -21,9 +21,17 @@ export default function App() {
           <a
             href="https://github.com/WickedZ2309/shecodes-react-week5-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://shecodes-react-week5-weather-app-ag.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
           .
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -6,6 +6,7 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -19,12 +20,6 @@ export default function Weather(props) {
     });
   }
 
-  function search() {
-    const apiKey = `2684689779a1e8c6910997393908cda2`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -32,6 +27,12 @@ export default function Weather(props) {
 
   function handleCityChange(event) {
     setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = `2684689779a1e8c6910997393908cda2`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
